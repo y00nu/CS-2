4e03114f7139059822608dabe2c0bace9293825d
Sangwoo Ryan Kim <zaq12wsx41@gmail.com>
2020-02-20 16:35:39 -0600

    Feb 20 lecture

--- prog8.cpp
+++ prog8.cpp
@@ -1,12 +1,16 @@
-// Feb 13, Feb 18 Linked List
+// Feb 13, Feb 18, Feb 20 Linked List
 /*
     저번 dynamic array는 사이즈가 다 찰때마다 옮겨야되서 비효율적이었다.
     Linked List로 조금더 효율적으로 구현.
     객체(노드)에 값과, 다음 객체(노드)의 주소가 들어간다.
     메모리에 0값은 NULL이라고 함. (물론 0 써도 인식함)
 */
-// 왜 포인터로 할수밖에 없는지? 그냥 객체 자체를 또 다른 객체안에 넣을수 없는건가?
+/*
+    포인터로 하는 이유는 프로그램이 실행중일때 새로운 객체, 변수할당 등을 할 수 있는 방법이 포인터 밖에 없어서 그렇다.
+*/
+
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 class Node{
 	private:
@@ -102,38 +106,130 @@ class List{
 		}
 
 		void remove(int x){
+            Node* currentNode = front;
+            Node* nodeBefore;
+            Node* nodeAfter;
+
+            for(int i = 0; i < size; i++){
+                if((*currentNode).getElement() == x){
+                    nodeAfter = (*currentNode).getNext();
+                    (*nodeBefore).setNext(nodeAfter);
+                    delete currentNode;
+                    break;
+                }
+                nodeBefore = currentNode;
+                currentNode = (*currentNode).getNext();
+            }
 		}
 
 		void removeFront(){
+            Node* frontNode = front;
+            Node* secondNode = frontNode -> getNext(); // How to access to the element of the pointer (Pointer notation)
+
+            delete frontNode;
+
+            front = secondNode;
+            size --;
 		}
 
 		void removeLast(){
-		}
+            Node* lastNode;
+            Node* nodeBefore = front;
+
+            for(int i = 0; i<size-2; i++){
+                nodeBefore = nodeBefore -> getNext();
+            }
+            nodeBefore->setNext(NULL);
+            lastNode = nodeBefore->getNext();
+            delete lastNode;
+            size --;
+        }
 
 		void print(){
             Node* temp;
             temp = front;
-
             for(int i = 0; i < size; i++){
                 cout << (*temp).getElement() << endl;
                 temp = (*temp).getNext();
             }
+            cout << "************" << endl;
 		}
 
 };
+
+class MultiList{
+    private:
+        List** hook;
+        int size;
+        int nbOfLists;
+    public:  
+        int getSize(){ return size; }
+        void setSize(int x){ size = x; }
+        int getNbOfLists(){ return nbOfLists; }
+        void setNbOfLists(int x){ nbOfLists = x;}
+        List** getHook(){return hook;}
+        void setHook(List** p){hook = p;}
+        MultiList(int x = 2){
+            size = 0;
+            nbOfLists = x;
+            hook = new List*[nbOfLists]; // nbOfLists 만큼의 list를 할당할 메모리를 만들겠다라는 의미
+            
+        }
+
+        void addElement(int x){
+
+        }
+
+        void removeElement(int x){
+
+        }
+
+        void print(){
+
+        }
+};
 int main(){
 	List obj1;
-	for (int i = 0; i<4; i++){
-        obj1.addFront(10+i*100);
-        obj1.addLast(20 + 100 * i);
-    }  
+    // Add 5 nodes to node list
+	for (int i = 5; i>=1; i--){
+        obj1.addFront(i);
+    } 
     obj1.print();
+
     Node* temp=obj1.getFront();
-    for (int j=0; j<4; j++){
+    // Hold the 4th node
+    for (int j=0; j<3; j++){
         temp=(*temp).getNext();
     }
+
+    // Add after 4th node
     obj1.addAfter(5000,temp);
     obj1.print();
+
+    // Add before 4th node
+    obj1.addBefore(1000,temp);
+    obj1.print();
+
+    // remove last node
+    obj1.removeLast();
+    obj1.print();
+
+    // remove front node
+    obj1.removeFront();
+    obj1.print();
+
+    // remove 4
+    obj1.remove(4);
+    obj1.print();
+
+
+    MultiList obj2(4);
+    for (int i = 0; i < 20; i++)
+    {
+        obj2.addElement(rand()%501);
+    }
+
+    obj2.print();
 	return 0;
 }
 
